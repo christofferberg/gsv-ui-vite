d2ea936cb3f819db6c034e0cf599569c8b91e2b8
Christoffer Berg <christofferbergj@gmail.com>
2021-05-18 13:40:58 +0200

    Some shit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,7 @@
-import path from 'path'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  build: {
-    target: ['chrome80'],
-    lib: {
-      name: 'gsv-ui',
-      fileName: 'index',
-      entry: path.resolve(__dirname, 'src/main.ts'),
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-        },
-      },
-    },
-  },
 })
